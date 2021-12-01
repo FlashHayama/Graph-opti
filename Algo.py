e75c891dae2d7b199a4a355a82dbe123ce67254d
Marchand Valentin <marchandvalentin.7@outlook.fr>
2021-12-01 08:58:18 +0100

    start

---
+++ Algo.py
@@ -0,0 +1,16 @@
+class Algo:
+
+    def __init__(N,B,E,code):
+        self.N
+        self.B
+        self.E
+        self.nbrs
+        self.code
+        self.bestSol
+        self.matDiv
+
+    def division():
+        pass
+
+    def fusion():
+        pass
