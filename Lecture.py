47cb9c8dd6e4cf604a59bf508eb3ca0e8415413c
valentinGailliez <63590566+valentinGailliez@users.noreply.github.com>
2021-12-01 11:06:55 +0100

    lecture de fichier

---
+++ Lecture.py
@@ -0,0 +1,24 @@
+class Lecture:
+    def __init__(self) -> None:
+        pass
+
+    def lecture_donnee():
+        with open("data/data1.dat","r") as f:
+            nbrs = list()
+            is_integer = 0
+            for line in f:
+                if "#" in line:
+                    # on saute la ligne
+                    continue
+                data = line.split()
+                if is_integer<3:
+                    if is_integer==0:
+                        N=data[0]
+                    else:
+                        if is_integer==1:
+                            E=data[0]
+                        else:
+                            B=data[0]
+                    is_integer+=1
+                else:
+                    nbrs.append(data[0])
