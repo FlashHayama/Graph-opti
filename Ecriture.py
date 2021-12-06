1042f1d279973a1488330e8ecd7ee3a17afab1d3
valentinGailliez <63590566+valentinGailliez@users.noreply.github.com>
2021-12-06 11:29:07 +0100

    ecriture dans un fichier

---
+++ Ecriture.py
@@ -0,0 +1,23 @@
+from Algo import Algo
+from Tri import Tri
+class Ecriture:
+    def __init__(self) -> None:
+        pass
+
+    def ecriture_donnee(self,a):
+        file = open("challenge/GroupeG1.txt","w")
+        for i in range(a.N):
+            file.write(str(i+1)+"\t"+str(a.nbrs[i])+"\t"+str(a.bestCode[i])+"\t")
+            for j in range(a.bestCode[i]):
+                file.write(str(a.BestMatDiv[i][j])+" ")
+
+            file.write("\n")
+        t = Tri()
+        tabTrie = t.fusion(a.BestMatDiv)
+        for iterB in range(a.B):
+            file.write("B"+str(iterB+1)+"\t")
+           
+            file.write(str(tabTrie[a.E*iterB])+"\n")
+
+        file.write("CODE  "+str(a.bestSol))
+        file.close()
