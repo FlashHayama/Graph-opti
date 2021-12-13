b81bede68273cd44ee4fe9a7042a5845f3ce495a
valentinGailliez <63590566+valentinGailliez@users.noreply.github.com>
2021-12-13 11:24:27 +0100

    temps dexecution

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from Algo import Algo
 from Lecture import Lecture
 from Ecriture import Ecriture
+import time
 
+start = time.time()
 
 l = Lecture()
-l.lecture_donnee(6)
+l.lecture_donnee(1)
 print("Quantité de nombre à traiter :          ",l.N)
 print("Quntité de boite à créer :              ",l.B)
 print("Quantité d'emplacement dans les boite : ",l.E)
@@ -13,8 +15,14 @@ print(l.nbrs)
 a = Algo(l.N,l.B,l.E,l.nbrs)
 
 a.Variable_neighborhood(a.balanced_solution(),10000,a.basic_moove,a.moove1,a.inverse_moove,a.transposition_moove,a.random_moove)
-a.simalated_annealing(a.bestCode,a.basic_moove,a.random_moove,100,0.1,100,0.99)
+#a.simalated_annealing(a.bestCode,a.basic_moove,a.random_moove,100,0.1,100,0.99)
 
 e = Ecriture()
 e.ecriture_donnee(a)
 print(a)
+
+
+end = time.time()
+elapsed = end - start
+
+print(f'Temps d\'exécution : {elapsed:.2}ms')
